62b41a69c922f241a7705d11cbf2c0b17017aae5
Amanzhol <Amanzhol>
2017-02-16 16:48:48 +0600

    Changed watch event

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var batch = require('gulp-batch');
 var jade = require('gulp-jade');
 var postcss = require('gulp-postcss');
 var concatCss = require('gulp-concat-css');
@@ -18,100 +18,82 @@ gulp.task('server', function() {
             baseDir: 'build'
         },
         notify: true,
-        reloadDelay: 2000
+        reloadDelay: 3000
     });
 });
 
 
 gulp.task('templates', function() {
-    var YOUR_LOCALS = {};
-    return watch('./app/**/*.jade', function () {
-      gulp.src(['!app/layouts/**', './app/**/*.jade'])
-        .pipe(jade({
-            locals: YOUR_LOCALS,
-            pretty: true
-        }))
-        .pipe(gulp.dest('./build'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  var YOUR_LOCALS = {};
+  gulp.src(['!./app/layouts/**', './app/**/*.jade'])
+    .pipe(jade({
+        locals: YOUR_LOCALS,
+        pretty: true
+    }))
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('styles', function () {
-    return watch('./app/css/**/*.css', function () {
-      gulp.src('./app/css/**/*.css')
-        .pipe(postcss([ autoprefixer() ]))
-        .pipe(concatCss("app.css"))
-        .pipe(gulp.dest('./build/css'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/css/**/*.css')
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(concatCss("app.css"))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('ts', function () {
-    return watch('./app/scripts/ts/**/*.ts', function () {
-      gulp.src('./app/scripts/ts/**/*.ts')
-        .pipe(ts({
-            noImplicitAny: true,
-            out: 'tsApp.js'
-        }))
-        .pipe(gulp.dest('build/js'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/scripts/ts/**/*.ts')
+    .pipe(ts({
+        noImplicitAny: true,
+        out: 'tsApp.js'
+    }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('js', function () {
-    return watch('./app/scripts/js/**/*.js', function () {
-      gulp.src('./app/scripts/js/**/*.js')
-        .pipe(gulp.dest('build/js'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/scripts/js/**/*.js')
+    .pipe(gulp.dest('build/js'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('images', function () {
-    return watch('./app/img/**/*', function () {
-      gulp.src('./app/img/**/*')
-        .pipe(gulp.dest('build/img'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/img/**/*')
+    .pipe(gulp.dest('build/img'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('fonts', function () {
-    return watch('./app/fonts/**/*', function () {
-      gulp.src('./app/fonts/**/*')
-        .pipe(gulp.dest('build/fonts'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/fonts/**/*')
+    .pipe(gulp.dest('build/fonts'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('lib-scripts', function () {
-    return watch('./app/lib/scripts/**/*.js', function () {
-      gulp.src('./app/lib/scripts/**/*.js')
-        .pipe(gulp.dest('build/lib/scripts'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/lib/scripts/**/*.js')
+    .pipe(gulp.dest('build/lib/scripts'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('lib-styles', function () {
-    return watch('./app/lib/styles/**/*.css', function () {
-      gulp.src('./app/lib/styles/**/*.css')
-        .pipe(gulp.dest('build/lib/styles'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/lib/styles/**/*.css')
+    .pipe(gulp.dest('build/lib/styles'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 gulp.task('lib-fonts', function () {
-    return watch('./app/lib/fonts/**/*', function () {
-      gulp.src('./app/lib/fonts/**/*')
-        .pipe(gulp.dest('build/lib/fonts'))
-        .pipe(browserSync.reload({stream: true}));
-    });
+  gulp.src('./app/lib/fonts/**/*')
+    .pipe(gulp.dest('build/lib/fonts'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
@@ -121,24 +103,23 @@ gulp.task('delete', function() {
 
 
 gulp.task('clean', function () {
-    cache.clearAll();
+    cache.clearAll(); // Удаляем Кэш
+});
+
+gulp.task('watch', ['server', 'templates', 'styles', 'ts', 'js', 'images', 'fonts', 'lib-scripts', 'lib-styles', 'lib-fonts'], function () {
+  watch('./app/**/*.jade', batch(function (events, done) { gulp.start('templates', done); }));
+  watch('./app/css/**/*.css', batch(function (events, done) { gulp.start('styles', done); }));
+  watch('./app/scripts/ts/**/*.ts', batch(function (events, done) { gulp.start('ts', done); }));
+  watch('./app/scripts/js/**/*.js', batch(function (events, done) { gulp.start('js', done); }));
+  watch('./app/img/**/*', batch(function (events, done) { gulp.start('images', done); }));
+  watch('./app/fonts/**/*', batch(function (events, done) { gulp.start('fonts', done); }));
+  watch('./app/lib/scripts/**/*.js', batch(function (events, done) { gulp.start('lib-scripts', done); }));
+  watch('./app/lib/styles/**/*.css', batch(function (events, done) { gulp.start('lib-styles', done); }));
+  watch('./app/lib/fonts/**/*', batch(function (events, done) { gulp.start('lib-fonts', done); }));
 });
 
 
-gulp.task('run', ['server', 'templates', 'styles', 'ts', 'js', 'images', 'fonts', 'lib-scripts', 'lib-styles', 'lib-fonts'], function() {
-    // gulp.watch('./app/css/**/*.css', ['templates', 'styles', 'js', 'ts', 'images', 'fonts']).on("change", reload);
-    // gulp.watch('./app/**/*.jade', ['templates', 'styles', 'js', 'ts', 'images', 'fonts']).on("change", reload);
-    // gulp.watch('./app/scripts/ts/**/*.ts', ['templates', 'styles', 'js', 'ts', 'images', 'fonts']).on("change", reload);
-    // gulp.watch('./app/scripts/js/**/*.js', ['templates', 'styles', 'js', 'ts', 'images', 'fonts']).on("change", reload);
-    // gulp.watch('./app/img/**/*', ['images']).on("change", reload);
-    // gulp.watch('./app/fonts/**/*', ['fonts']).on("change", reload);
-    // gulp.watch('./app/lib/scripts/**/*.js', ['lib-scripts']).on("change", reload);
-    // gulp.watch('./app/lib/styles/**/*.css', ['lib-styles']).on("change", reload);
-    // gulp.watch('./app/lib/fonts/**/*', ['lib-fonts']).on("change", reload);
-});
+gulp.task('build', ['delete', 'clean', 'watch']);
 
 
-gulp.task('build', ['delete', 'clean', 'run']);
-
-
-gulp.task('default', ['run']);
+gulp.task('default', ['watch']);
